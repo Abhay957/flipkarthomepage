4e83528b30a30cca6d9191091ceb1d3b7a56d234
Abhay Gupta <abhaygupta957@gmail.com>
2022-12-27 11:30:07 +0530

    commit

--- appone.js
+++ appone.js
@@ -1,29 +1,5 @@
-var inputValue = document.querySelector("#input-text");
-var btn  = document.querySelector("#but");
-var output = document.querySelector("#out");
+var input = document.querySelector("#type");
+var btn = document.querySelector("#button");
+var output = documnet.querySelector("#out");
 
-var serverUrl = "https://nftbaazarapi.rawheatg.repl.co/product";
-
-function constructURL(text){
-    return serverUrl + "?" + "text=" + text;
-}
-
-function clickHandler(){
-    var value = inputValue.value;
-    fetch(serverUrl)
-    .then(response => response.json())
-    .then(data => {
-        for(var i=0; i<data.data.length; i=i+1){
-            if(Number(data.data[i].price) > 500){
-                
-                data.data[i].price.style.color = "red";
-            }
-        }
-    })
-}
-
-
-
-
-
-btn.addEventListener("click", clickHandler);
+btn.disabled = true;
