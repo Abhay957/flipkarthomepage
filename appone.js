77fe979ca4cbd10c071d198e894ceca4a2366912
Abhay Gupta <abhaygupta957@gmail.com>
2022-12-31 13:48:57 +0530

    x

--- appone.js
+++ appone.js
@@ -1,16 +1,20 @@
 var input = document.querySelector("#input-box");
-var b1 = document.querySelector("#b-one");
-var b2 = document.querySelector("#b-two");
+var bone = document.querySelector("#b-one");
+var btwo = document.querySelector("#b-two");
+var bthree = document.querySelector("#b-three");
 var output = document.querySelector("#out");
 
-var fontsize = 16;
-b1.addEventListener("click", () => {
-    fontsize = fontsize + 2;
+bone.addEventListener("click", () => {
     output.innerText = input.value;
-    output.style.fontSize = fontsize + "px";
+    output.style.color = "green";
 })
-b2.addEventListener("click", () => {
-    fontsize = fontsize - 2;
+
+btwo.addEventListener("click", () => {
     output.innerText = input.value;
-    output.style.fontSize = fontsize + "px";
+    output.style.color = "red";
+})
+
+bthree.addEventListener("click", () => {
+    output.innerText = input.value;
+    output.style.color = "blue";
 })
