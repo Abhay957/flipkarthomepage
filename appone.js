4ccb544734c98d30ce447e4b0166d92faef9991e
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-04 12:37:58 +0530

    d

--- appone.js
+++ appone.js
@@ -1,31 +1,20 @@
-
-var buyPrice = document.querySelector("#buying-price");
-var Name = document.querySelector("#stock-name");
-var output = document.querySelector("#out");
-var btn = document.querySelector("#fetch");
-
- var serverURL = "https://stock-api.desaihetav.repl.co/current-price/";
-
- function constructURL(stoc){
-    return serverURL + stoc;
- }
-
- btn.addEventListener("click", () => {
-    var stockname = Name.value;
-    stockname = stockname.toUpperCase();
-    fetch(constructURL(stockname))
-    .then(Response => Response.json())
-    .then(json => {
-        var crntPrice = json.current_price;
-        var price =  Number(buyPrice.value);
-        if(crntPrice > price){
-            var rsult = crntPrice - price;
-            output.innerText = "You are currently in profit of " + rsult;
-        }
-        else{
-            rsult = price - crntPrice;
-            output.innerText = "You are currently in loss of " + rsult;
-        }
-    })
-    .catch(erroe => output.innerText = "Error!")
- })
+var input = document.querySelector("#input-text");
+var btn = document.querySelector("#sub-btn");
+var output = document.querySelector("#out-div");
+btn.disabled = true;
+input.addEventListener("input",() => {
+    if(input.value.length >= 10){
+        btn.disabled = false;
+    }
+    else{
+        btn.disabled = true;
+    }
+})
+btn.addEventListener("click", () => {
+    if(input.value.length >= 10){
+        output.innerText = "Success";
+    }
+    else{
+        output.innerText = "Error";
+    }
+})
