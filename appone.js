6d854d22ee5a784415a989e67220bb81beb5921f
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-10 13:31:24 +0530

    f

--- appone.js
+++ appone.js
@@ -1,16 +1,27 @@
-var bone = document.querySelector("#one");
-var btwo = document.querySelector("#two");
-var bthree = document.querySelector("#three");
-var para = document.querySelector("#para");
+var input = document.querySelector("#input-password");
+var btn = document.querySelector("#btn-submit");
+var output = document.querySelector("#out-div");
 
-bone.addEventListener("click",() => {
-    para.style.fontFamily = 'Roboto', "sans-serif";
+btn.disabled = true;
+
+
+input.addEventListener("input",() => {
+    if(input.value.length >= 10){
+        btn.disabled = false;
+        input.style.backgroundColor = "green";
+    }
+    else{
+        btn.disabled = true;
+        input.style.backgroundColor = "red";
+    }
 })
 
-btwo.addEventListener("click",() => {
-    para.style.fontFamily = 'Seymour One', "sans-serif";
-})
 
-bthree.addEventListener("click", () => {
-    para.style.fontFamily = 'East Sea Dokdo', "cursive";
+btn.addEventListener("click",() => {
+    if(input.value.length >= 10){
+        output.innerText = "Success";
+    }
+    else{
+        output.innerText = "Error";
+    }
 })
