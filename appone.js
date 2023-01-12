a978fa281060c89e65bb3d876a28ef4e1d7c5884
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-12 11:45:48 +0530

    a

--- appone.js
+++ appone.js
@@ -1,27 +1,7 @@
-var input = document.querySelector("#input-password");
-var btn = document.querySelector("#btn-submit");
+var input = document.querySelector("#input-text");
+var btn = document.querySelector("#btn-sub");
 var output = document.querySelector("#out-div");
 
-btn.disabled = true;
-
-
-input.addEventListener("input",() => {
-    if(input.value.length >= 10){
-        btn.disabled = false;
-        input.style.backgroundColor = "green";
-    }
-    else{
-        btn.disabled = true;
-        input.style.backgroundColor = "red";
-    }
-})
-
-
 btn.addEventListener("click",() => {
-    if(input.value.length >= 10){
-        output.innerText = "Success";
-    }
-    else{
-        output.innerText = "Error";
-    }
+    output.innerHTML = `<p><strong style="color:blue" >${input.value}</strong>${" " + input.value.length}</p>`;
 })
