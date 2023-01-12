39d21383dc169444902cbd56fc80e2a234a7576a
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-12 15:31:37 +0530

    x

--- appone.js
+++ appone.js
@@ -1,7 +1,36 @@
-var input = document.querySelector("#input-text");
-var btn = document.querySelector("#btn-sub");
+var btn = document.querySelector("#btn-click");
 var output = document.querySelector("#out-div");
-
+var output2 = document.querySelector("#out-divv");
+var data = [
+    {
+        name:"Abhgay",
+        age:23,
+        gender:"male"
+    },
+    {
+        name:"ramdevi",
+        age:136,
+        gender:"male"
+    },
+    {
+        name:"ANNANANAnnn",
+        age:34,
+        gender:"female"
+    }
+];
 btn.addEventListener("click",() => {
-    output.innerHTML = `<p><strong style="color:blue" >${input.value}</strong>${" " + input.value.length}</p>`;
+    for(var i=0;i<data.length;i++){
+        if(data[i].name.length > 5 && data[i].age > 18 ){
+             
+            if(data[i].gender === "male"){
+                output.innerText += data[i].name + "\n";
+                output.style.color = "red";
+
+            }
+            else{
+                output2.innerText += data[i].name + "\n";
+                output2.style.color = "blue";
+            }
+        } 
+    }
 })
