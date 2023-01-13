626b0bf888e9e1da776cf2c8f401c26fab0c8266
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-13 08:16:46 +0530

    c

--- appone.js
+++ appone.js
@@ -1,36 +1,16 @@
-var btn = document.querySelector("#btn-click");
-var output = document.querySelector("#out-div");
-var output2 = document.querySelector("#out-divv");
-var data = [
-    {
-        name:"Abhgay",
-        age:23,
-        gender:"male"
-    },
-    {
-        name:"ramdevi",
-        age:136,
-        gender:"male"
-    },
-    {
-        name:"ANNANANAnnn",
-        age:34,
-        gender:"female"
-    }
-];
-btn.addEventListener("click",() => {
-    for(var i=0;i<data.length;i++){
-        if(data[i].name.length > 5 && data[i].age > 18 ){
-             
-            if(data[i].gender === "male"){
-                output.innerText += data[i].name + "\n";
-                output.style.color = "red";
+var p = document.querySelector("#ppp");
+var bone = document.querySelector("#b-one");
+var btwo = document.querySelector("#b-two")
+var bthree = document.querySelector("#b-three");
 
-            }
-            else{
-                output2.innerText += data[i].name + "\n";
-                output2.style.color = "blue";
-            }
-        } 
-    }
+bone.addEventListener("click",() => {
+    p.style.fontFamily = "'Rubik Bubbles', cursive";
+})
+
+btwo.addEventListener("click", () => {
+    p.style.fontFamily = "'Roboto', sans-serif";
+})
+
+bthree.addEventListener("click",() => {
+    p.style.fontFamily = "'Montserrat', sans-serif";
 })
