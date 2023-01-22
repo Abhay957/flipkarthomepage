1ac245e1f7909ae443d505865f46e0a6d1233451
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-22 13:45:37 +0530

    c

---
+++ appone.js
@@ -0,0 +1,13 @@
+console.time("abhay");
+for(let i=0;i<5;i++){
+    console.log("Abhay");
+}
+console.timeEnd("abhay");
+
+console.time("abh");
+let i=0;
+while(i!=5){
+    console.log("abhay");
+    i++;
+}
+console.timeEnd("abh");
