5e85d31794bdec03ef4878064f6a27a6ab200b49
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-23 16:04:18 +0530

    x

--- appone.js
+++ appone.js
@@ -1,14 +1,13 @@
 let random = Math.floor(Math.random()*3);
 let cg = ["s","w","g"];
 cg = cg[random];
-let ug = prompt("Enter either snake water or gun");
-
+let ug = prompt("Enter your guesss");
 const match = (cg,ug) => {
     if(cg == ug){
         return "tie";
     }
     else if(cg == "s" && ug == "w"){
-        return "Computer wins!";
+        return "Computer wins";
     }
     else if(cg == "s" && ug == "g"){
         return "User wins";
@@ -27,4 +26,65 @@ const match = (cg,ug) => {
     }
 }
 let result = match(cg,ug);
-document.write(`Computer played: ${cg} \n user played: ${ug}\n Hence ${result} the match`);
+document.write(`Result is ${result}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let random = Math.floor(Math.random()*3);
+// let cg = ["s","w","g"];
+// cg = cg[random];
+// let ug = prompt("Enter either snake water or gun");
+
+// const match = (cg,ug) => {
+//     if(cg == ug){
+//         return "tie";
+//     }
+//     else if(cg == "s" && ug == "w"){
+//         return "Computer wins!";
+//     }
+//     else if(cg == "s" && ug == "g"){
+//         return "User wins";
+//     }
+//     else if(cg == "w" && ug == "s"){
+//         return "User wins";
+//     }
+//     else if(cg == "w" && ug == "g"){
+//         return "Computer wins";
+//     }
+//     else if(cg == "g" && ug == "s"){
+//         return "Computer wins";
+//     }
+//     else if(cg == "g" && ug == "w"){
+//         return "User wins";
+//     }
+// }
+// let result = match(cg,ug);
+// document.write(`Computer played: ${cg} \n user played: ${ug}\n Hence ${result} the match`);
