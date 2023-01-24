b692fb38cf93d8ce0c804e6c2bf32163c4866fb3
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-24 11:43:20 +0530

    d

--- appone.js
+++ appone.js
@@ -1,90 +1,20 @@
-let random = Math.floor(Math.random()*3);
-let cg = ["s","w","g"];
-cg = cg[random];
-let ug = prompt("Enter your guesss");
-const match = (cg,ug) => {
-    if(cg == ug){
-        return "tie";
-    }
-    else if(cg == "s" && ug == "w"){
-        return "Computer wins";
-    }
-    else if(cg == "s" && ug == "g"){
-        return "User wins";
-    }
-    else if(cg == "w" && ug == "s"){
-        return "User wins";
-    }
-    else if(cg == "w" && ug == "g"){
-        return "Computer wins";
-    }
-    else if(cg == "g" && ug == "s"){
-        return "Computer wins";
-    }
-    else if(cg == "g" && ug == "w"){
-        return "User wins";
-    }
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
 }
-let result = match(cg,ug);
-document.write(`Result is ${result}`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let random = Math.floor(Math.random()*3);
-// let cg = ["s","w","g"];
-// cg = cg[random];
-// let ug = prompt("Enter either snake water or gun");
-
-// const match = (cg,ug) => {
-//     if(cg == ug){
-//         return "tie";
-//     }
-//     else if(cg == "s" && ug == "w"){
-//         return "Computer wins!";
-//     }
-//     else if(cg == "s" && ug == "g"){
-//         return "User wins";
-//     }
-//     else if(cg == "w" && ug == "s"){
-//         return "User wins";
-//     }
-//     else if(cg == "w" && ug == "g"){
-//         return "Computer wins";
-//     }
-//     else if(cg == "g" && ug == "s"){
-//         return "Computer wins";
-//     }
-//     else if(cg == "g" && ug == "w"){
-//         return "User wins";
-//     }
-// }
-// let result = match(cg,ug);
-// document.write(`Computer played: ${cg} \n user played: ${ug}\n Hence ${result} the match`);
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Nice, you triggered this alert message!', 'success')
+  })
+}
