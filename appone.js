b60d86ce46e001c2b0fc5b8dc5db9bb2d3cc8a07
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-26 14:48:41 +0530

    c
    c

--- appone.js
+++ appone.js
@@ -1,8 +1,2 @@
-let a = document.body.getElementsByTagName('div')[0];
-
-let di = document.createElement("h1");
-
-di.innerHTML = 'Hello lets check whether its working or not';
-
-// a.prepend(di);
- a.replaceWith(di);
+let div = document.getElementById("first");
+div.insertAdjacentHTML("beforeend","<span> my name is abhay</span>")
