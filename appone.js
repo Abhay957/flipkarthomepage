d13c281a1649cfad5b689c93e11dec63486bd528
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-28 11:39:12 +0530

    s
    s

--- appone.js
+++ appone.js
@@ -1,15 +1,133 @@
+let promise1 = new Promise((resolve,reject) => {
+    
+    setTimeout(() => {
+        resolve(true);
+    },5000)
+})
 
-let clock = () => {
-    let a = new Date();
-    let d = a.getDate();
-    let mon = a.getMonth();
-    let h = a.getHours();
-    let m = a.getMinutes();
-    let s = a.getSeconds();
-    let clk = document.getElementById("clock");
-    let time = document.getElementById("time");
-    clk.innerHTML= `<span class="d">Date:</span> ${d}`;
-    time.innerHTML = `<span class="t" >Time:</span> ${h}:${m}:${s}`;
-}
 
-setInterval(clock,2000);
+let promise2 = new Promise((resolve,reject) => {
+    
+    setTimeout(() => {
+        reject(new Error("I am promise and i am rejected"));
+    },5000);
+})
+promise1.then((value) => {
+    console.log(value);
+})
+promise2.then((value) => {
+    console.log(value);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function loadscript(source,callback){
+//     let script = document.createElement("script");
+//     script.src = source;
+//     document.body.append(script);
+//     script.onload = () => {
+//         console.log("Script loaded with source " + source);
+//         callback(null,source);
+//     }
+//     script.onerror = () => {
+//         console.log("Error loading script");
+//         callback(new Error("Failed to load script"),source);
+//     }
+
+// }
+
+// loadscript("https://ccdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",(err ,source) => {
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     loadscript("https://ccdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",(err ,source) => {
+//         if(err){
+//             console.log(err);
+//             return;
+//         }))
+//         loadscript("https://ccdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",)
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function loadscript(source,callback){
+//     let script = document.createElement("script");
+//     script.src = source;
+//     script.onload = () => {
+//         console.log("Hey i am good");
+//         callback(source);
+//     }
+//     document.body.append(script);
+// }
+// source = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js";
+// const hello = (source) => {
+//     alert('Hello script executed' + source);
+// }
+
+
+// loadscript(source,hello);
