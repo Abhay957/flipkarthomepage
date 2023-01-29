006443d583af10b6dffd2c0c28b4e6c17a0f4b27
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-29 11:27:39 +0530

    d

--- appone.js
+++ appone.js
@@ -1,133 +1,112 @@
-let promise1 = new Promise((resolve,reject) => {
-    
-    setTimeout(() => {
-        resolve(true);
-    },5000)
-})
+// let p1 = new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//         resolve(1);
+//     },11000)
+// })
+
+// let p2 = new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//         // resolve(2);
+//         reject(new Error("Error loading this"));
+//     },2000)
+// })
+
+// let p3 = new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//         resolve(3);
+//     },3000)
+// })
+
+// p1.then((value) => {
+//     console.log(value)
+// })
+
+// p2.then((value) => {
+//     console.log(value);
+// })
+
+// p3.then((value) => {
+//     console.log(value);
+// })
+
+// let promise_all = Promise.all([p1,p2,p3]);
+
+// promise_all.then((value) => {
+//     console.log(value);
+// } ).catch((error) => {
+//     console.log("Error we are not able to load this page");
+//     console.log(error);
+// } )
+
+
+// let promise_allSetteled = Promise.allSettled([p1,p2,p3]);
+
+// promise_allSetteled.then((value) => {
+//     console.log(value);
+// })
+
+
+// let promise_race = Promise.race([p1,p2,p3]);
+
+// promise_race.then((value) => {
+//     console.log(value);
+// })
+
+// let promise_any = Promise.any([p1,p2,p3]);
+
+// promise_any.then((va) => {
+//     console.log(va);
+// })
+
+// let promise_resolve = Promise.resolve(123);
+// promise_resolve.then((val) => {
+//     console.log(val);
+// })
+
+
+// let promise_reject = Promise.reject(new Error("We are using reject method from the promise object"));
+// promise_reject.then((e) => {
+//     console.log(e);
+// }).catch((error)=> {
+//     console.log(error);
+// })
+
+async function abhay(){
+    let delhiWeather = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve("27deg");
+        },2000)
+    })
+    let banWeather = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve("21deg");
+        },5000)
+    })
+    // delhiWeather.then(alert);
+    // banWeather.then(alert);
+    console.log("Fetching delhi weather");
+    let delhiW = await delhiWeather;
+    console.log("Delhi weather has been fetched");
+    console.log("Fetching bangalore weather");
+    let bangW = await banWeather;
+    console.log("Bangalore weather has been fetched");
+    return [delhiW,bangW]
+}
+// abhay();
+const cherry = async () => {
+    console.log("I will get executed when on time execution lines of the abhay will be executed ")
+}
+const wait = async () => {
+    let a = await abhay();
+    console.log("Cherry will be executed after abhay has completed its execution 100%");
+    let b = await cherry();
+    console.log(b);
+    // a.then((value) => {
+    //    console.log(value);
+    // })
+}
 
+wait();
 
-let promise2 = new Promise((resolve,reject) => {
-    
-    setTimeout(() => {
-        reject(new Error("I am promise and i am rejected"));
-    },5000);
-})
-promise1.then((value) => {
-    console.log(value);
-})
-promise2.then((value) => {
-    console.log(value);
-})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function loadscript(source,callback){
-//     let script = document.createElement("script");
-//     script.src = source;
-//     document.body.append(script);
-//     script.onload = () => {
-//         console.log("Script loaded with source " + source);
-//         callback(null,source);
-//     }
-//     script.onerror = () => {
-//         console.log("Error loading script");
-//         callback(new Error("Failed to load script"),source);
-//     }
-
-// }
-
-// loadscript("https://ccdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",(err ,source) => {
-//     if(err){
-//         console.log(err);
-//         return;
-//     }
-//     loadscript("https://ccdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",(err ,source) => {
-//         if(err){
-//             console.log(err);
-//             return;
-//         }))
-//         loadscript("https://ccdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",)
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function loadscript(source,callback){
-//     let script = document.createElement("script");
-//     script.src = source;
-//     script.onload = () => {
-//         console.log("Hey i am good");
-//         callback(source);
-//     }
-//     document.body.append(script);
-// }
-// source = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js";
-// const hello = (source) => {
-//     alert('Hello script executed' + source);
-// }
-
-
-// loadscript(source,hello);
