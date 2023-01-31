d323525165f9c1707971a4a0634707caa5852d8a
Abhay Gupta <abhaygupta957@gmail.com>
2023-01-31 15:43:35 +0530

    s

--- appone.js
+++ appone.js
@@ -1,6 +1,31 @@
-try{
-    console.log("Executed);
+const abhay = async (hehe) => {
+    let options = {
+        method:"POST",
+        headers:{
+        "Content-type":"application/json"
+    },
+        body: JSON.stringify(hehe)
 }
-catch(error){
-    console.log("Error",error);
+    let f = await fetch("https://jsonplaceholder.typicode.com/posts",options);
+    let val = f.json();
+    return val;
 }
+
+let getr = async (id) => {
+    let h = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
+    let r = await h.json();
+    return r;
+}
+
+let a = async () => {
+    let hehe =
+    {
+            title: 'Abhay',
+            body: 'Hello',
+            userId: 1,
+    }
+    let b = await abhay(hehe);
+    console.log(b);
+    console.log(await getr(24));
+}
+a();
